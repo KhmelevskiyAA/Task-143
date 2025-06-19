400f506f9280fa66d09e7c569a3bc833bac6d628
Хмелевский Артём Александрович ТСА-143 <160860313+KhmelevskiyAA@users.noreply.github.com>
2025-06-19 09:30:55 +0300

    Update jdoodle (2).cpp

--- jdoodle (2).cpp
+++ jdoodle (2).cpp
@@ -3,29 +3,6 @@
 #include <iomanip>
 
 using namespace std;
-
-/**
- * @brief Вычисляет значение A по формуле ∛(x*y*z + |z*sin(y)|)
- * @param x Первый параметр
- * @param y Второй параметр
- * @param z Третий параметр
- * @return Результат вычисления
- */
-double calculateA(const double x, const double y, const double z) {
-    return cbrt(x * y * z + abs(z * sin(y)));
-}
-
-/**
- * @brief Вычисляет значение B по формуле y*cos(x*z*sin(y)) + 3
- * @param x Первый параметр
- * @param y Второй параметр
- * @param z Третий параметр
- * @return Результат вычисления
- */
-double calculateB(const double x, const double y, const double z) {
-    return y * cos(x * z * sin(y)) + 3.0;
-}
-
 /**
  * @brief Точка входа в программу
  * @return 0 при успешном выполнении
@@ -43,4 +20,28 @@ int main() {
     cout << "B = " << calculateB(x, y, z) << endl;
 
     return 0;
+}
+
+/**
+ * @brief Вычисляет значение A (необходимо реализовать)
+ * @param x Первый параметр
+ * @param y Второй параметр
+ * @param z Третий параметр
+ * @return Результат вычисления
+ */
+double calculateA(const double x, const double y, const double z) {
+    // Реализация функции calculateA
+    // (добавьте свою формулу здесь)
+    return 0.0;
+}
+
+/**
+ * @brief Вычисляет значение B по формуле y*cos(x*z*sin(y)) + 3
+ * @param x Первый параметр
+ * @param y Второй параметр
+ * @param z Третий параметр
+ * @return Результат вычисления
+ */
+double calculateB(const double x, const double y, const double z) {
+    return y * cos(x * z * sin(y)) + 3.0;
 }
